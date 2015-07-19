7c908a4336a1a175fd450a1d50dbfbfee1fec1b2
Steve Baker <tektsu@gmail.com>
2015-07-19 09:53:26 -0700

    Initial project

---
+++ decx/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  decx
+//
+//  Created by Steve Baker on 7/19/15.
+//  Copyright (c) 2015 Steve Baker. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
