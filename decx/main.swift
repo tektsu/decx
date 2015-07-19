2334c8be0f220686f290edbf67bb3fb0595dbe93
Steve Baker <tektsu@gmail.com>
2015-07-19 14:02:12 -0700

    Add eEntry class

--- decx/main.swift
+++ decx/main.swift
@@ -1,3 +1,4 @@
+#!/usr/bin/env xcrun swift -F /Library/Frameworks
 //
 //  main.swift
 //  decx
@@ -7,6 +8,31 @@
 //
 
 import Foundation
+//import Darwin
 
-println("Hello, World!")
+struct Opts {
+  static let allCardsFile = "~/Development/decx/AllCards-x.json"
+}
 
+var error: NSError?
+let fileContent = NSString(contentsOfFile: Opts.allCardsFile.stringByExpandingTildeInPath, encoding: NSUTF8StringEncoding, error: nil)
+if (fileContent == nil) {
+  println("Unable to read file: " + Opts.allCardsFile)
+  exit(0)
+}
+//println(fileContent)
+
+var jsonError: NSError?
+let json = NSJSONSerialization.JSONObjectWithData(fileContent!.dataUsingEncoding(NSUTF8StringEncoding)!, options: nil, error: &jsonError) as! NSDictionary
+if (jsonError != nil) {
+  println("Error parsing JSON")
+  exit(0)
+}
+
+//println(json["Air Elemental"])
+
+for key in json.allKeys {
+  //println(json.objectForKey(key)!)
+  let entry = Entry(card: json.objectForKey(key)!)
+  println(entry.getName() + ", " + entry.getColor() + ", " + entry.getType() + ", " + entry.getSupertype())
+}
