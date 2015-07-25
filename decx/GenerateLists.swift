f6c959948d8f72ab8853df68c97c70bda1e2b214
Steve Baker <tektsu@gmail.com>
2015-07-25 11:55:56 -0700

    Write lists to files

--- decx/GenerateLists.swift
+++ decx/GenerateLists.swift
@@ -11,6 +11,8 @@ import Foundation
 class GenerateListsCommand: Command {
 
   private var allCardsFile: String = "AllCards-x-json"
+  private var writeListsToFiles = false
+  private var outputDirectory = "."
 
   override func commandName() -> String  {
     return "generate_lists"
@@ -24,11 +26,19 @@ class GenerateListsCommand: Command {
     onKeys(["-a", "--all-cards-file"], usage: "path to all-cards json file", valueSignature: "allCards") {(key, value) in
       self.allCardsFile = value;
     }
+    onKeys(["-o", "--output-directory"], usage: "path to file output directory", valueSignature: "outputDirectory") {(key, value) in
+      var checkValidation = NSFileManager.defaultManager()
+      if (!checkValidation.fileExistsAtPath(value)) {
+        println("Directory does not exist: " + value)
+        exit(1)
+      }
+      self.writeListsToFiles = true;
+      self.outputDirectory = value;
+    }
   }
 
   override func execute() -> ExecutionResult {
-    println(self.allCardsFile)
-    var listApp = GenerateLists(pathToFile: self.allCardsFile)
+    var listApp = GenerateLists(pathToFile: self.allCardsFile, writeListsToFiles: self.writeListsToFiles, outputDirectory: self.outputDirectory)
     listApp.execute()
     return success()
   }
@@ -38,9 +48,13 @@ class GenerateListsCommand: Command {
 class GenerateLists {
 
   private var allCardsFile = ""
+  private var writeListsToFiles = false;
+  private var outputDirectory = ""
 
-  init(pathToFile: String) {
+  init(pathToFile: String, writeListsToFiles: Bool, outputDirectory: String) {
     self.allCardsFile = pathToFile
+    self.writeListsToFiles = writeListsToFiles
+    self.outputDirectory = outputDirectory
   }
 
   private func outputListsToScreen(lists: EntryList) {
@@ -54,8 +68,23 @@ class GenerateLists {
     }
   }
 
+  private func outputListsToFiles(lists: EntryList) {
+    let listNames = lists.getListNames()
+    for listName in listNames {
+      let cardNames = lists.getListForName(listName)
+      let fileName = "\(outputDirectory)/mtgcards-\(listName).txt"
+      println(fileName)
+      (cardNames as NSArray).writeToFile(fileName, atomically: false)
+    }
+  }
+
   private func outputLists(lists: EntryList) {
-    outputListsToScreen(lists)
+    if (writeListsToFiles) {
+      outputListsToFiles(lists)
+    }
+    else {
+      outputListsToScreen(lists)
+    }
   }
   
   func execute() {
@@ -75,7 +104,5 @@ class GenerateLists {
     }
 
     outputLists(cardLists)
-
   }
-  
 }
