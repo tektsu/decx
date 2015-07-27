9cf879aa2aafad775f13c0cc49a8edd50c49a909
Steve Baker <tektsu@gmail.com>
2015-07-27 06:43:48 -0700

    Download all-cards file automatically

--- decx/RcDirectory.swift
+++ decx/RcDirectory.swift
@@ -20,6 +20,30 @@ class RcDirectory {
     }
   }
 
+  private func downloadFile(url: String, fileName: String) -> Bool {
+
+    //var url = NSURL(string: url)
+    var request = NSURLRequest(URL: NSURL(string: url)!)
+    var response: NSURLResponse?
+    var error: NSError?
+    var data = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &error)
+    if (error != nil) {
+      return false
+    }
+
+    var httpResponse = response as! NSHTTPURLResponse
+    if httpResponse.statusCode != 200 {
+      return false
+    }
+
+    data!.writeToFile(fileName, atomically: true)
+    return true
+  }
+
+  private func downloadAllCards() {
+    downloadFile("http://mtgjson.com/json/AllCards-x.json", fileName: pathToAllCards())
+  }
+
   private func pathToAllCards() -> String {
     let path = "\(topDirectory)/AllCards-x.json"
     return path
@@ -28,7 +52,8 @@ class RcDirectory {
   func getPathToAllCards() -> String {
     let path = pathToAllCards()
     var checkValidation = NSFileManager.defaultManager()
-    if (!checkValidation.fileExistsAtPath(topDirectory)) {
+    if (!checkValidation.fileExistsAtPath(path)) {
+      downloadAllCards()
     }
     return path
   }
