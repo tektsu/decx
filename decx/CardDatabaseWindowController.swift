e3522f1b104a888a00f816e742206273c22e927f
Steve Baker <tektsu@gmail.com>
2015-09-07 14:41:04 -0700

    Basic database search

--- decx/CardDatabaseWindowController.swift
+++ decx/CardDatabaseWindowController.swift
@@ -8,7 +8,12 @@
 
 import Cocoa
 
-class CardDatabaseWindowController: NSWindowController {
+class CardDatabaseWindowController: NSWindowController, NSTableViewDataSource {
+
+  @IBOutlet weak var searchField: NSSearchField!
+
+  @IBOutlet weak var dbView: NSTableView!
+  var dbViewData: [String] = []
 
   override var windowNibName: String? {
     return "CardDatabaseWindowController"
@@ -17,6 +22,30 @@ class CardDatabaseWindowController: NSWindowController {
   override func windowDidLoad() {
     super.windowDidLoad()
     var dataStore = DataStoreSQL.sharedInstance
+    dbView.reloadData()
   }
-    
+
+  @IBAction func doSearch(sender: AnyObject) {
+    dbViewData = []
+    if searchField.stringValue != "" {
+      println("Ping! \(searchField.stringValue)")
+
+      let dataStore = DataStoreSQL.sharedInstance
+      let db = dataStore.getConnection()!
+      let statement = db.prepare("SELECT cardName, cardColor, cardType FROM cards WHERE cardName LIKE ? OR cardText LIKE ? ORDER BY cardName", "%\(searchField.stringValue)%", "%\(searchField.stringValue)%")
+      for row in statement {
+        dbViewData.append(row[0] as! String)
+      }
+    }
+    dbView.reloadData()
+  }
+
+  func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+    return dbViewData.count
+  }
+
+  func tableView(tableView: NSTableView, objectValueForTableColumn tableColumn: NSTableColumn?, row: Int) -> AnyObject? {
+    return dbViewData[row]
+  }
+
 }
